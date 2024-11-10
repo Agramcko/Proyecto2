721ec343323057194d92a85f0e221e52a85e1016
Alessandro Gramcko <107635555+Agramcko@users.noreply.github.com>
2024-11-10 12:11:46 -0400

    Arreglos

--- src/proyecto2/Nodo.java
+++ src/proyecto2/Nodo.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package BODY;
+package proyecto2;
 
 /**
  *
  * @author gramc
  */
-public class Tree {
+public class Nodo {
     
 }
