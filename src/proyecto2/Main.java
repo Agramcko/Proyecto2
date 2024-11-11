32e08473cf55f0a10ea163f75a2b8fdcaa22e7e3
josesaggiomo <jose.saggiomo@correo.unimet.edu.ve>
2024-11-11 12:34:15 -0400

    se elimino

--- src/proyecto2/Main.java
+++ src/proyecto2/Main.java
@@ -14,8 +14,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       numero = 5;
-        System.out.println("");
+
     }
     
 }
